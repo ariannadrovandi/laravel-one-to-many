55139f2f16f6aa3cab1ad568f2ba9b6a083b5f23
ariannadrovandi <ariannadrovandi02@gmail.com>
2023-06-09 19:34:04 +0200

    migration seeder model request Type

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,9 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Project extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'slug', 'image', 'description'];
+
+    public function type(){
+        return $this->belongsTo(Type::class);
+    }
+
 }
